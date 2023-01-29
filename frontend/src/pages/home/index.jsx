01c3ba68b6486508ce4123b95ce2e86c7a0d4019
introbond <introbond.dev@gmail.com>
2023-01-29 08:11:56 +0700

    add logic: join room

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,37 @@
 import styles from './styles.module.css';
+import { useNavigate } from 'react-router-dom'; // Add this
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+
+    const navigate = useNavigate(); // Add this
+
+    const joinRoom = () => {
+        if (room !== '' && username !== '') {
+            socket.emit('join_room', { username, room });
+        }
+
+        // Redirect to /chat
+        navigate('/chat', { replace: true }); // Add this
+
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.formContainer}>
                 <h1>{`<>DevRooms</>`}</h1>
                 <input className={styles.input} placeholder='Username...' />
+
+                <input
+                    className={styles.input}
+                    placeholder='Username...'
+                    onChange={(e) => setUsername(e.target.value)} // Add this
+                />
+
+                <select
+                    className={styles.input}
+                    onChange={(e) => setRoom(e.target.value)} // Add this
+                >
+                </select>
 
                 <select className={styles.input}>
                     <option>-- Select Room --</option>
