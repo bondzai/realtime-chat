6aecfa8da5a9a142793568d61e74bda6cae9aca1
introbond <introbond.dev@gmail.com>
2023-01-29 11:10:55 +0700

    added component send message

--- frontend/src/pages/chat/index.jsx
+++ frontend/src/pages/chat/index.jsx
@@ -1,14 +1,16 @@
 import styles from './styles.module.css';
 import MessagesReceived from './messages';
+import SendMessage from './send_message';
 
-const Chat = ({ socket }) => {
-  return (
-    <div className={styles.chatContainer}>
-      <div>
-        <MessagesReceived socket={socket} />
-      </div>
-    </div>
-  );
+const Chat = ({ username, room, socket }) => {
+    return (
+        <div className={styles.chatContainer}>
+            <div>
+                <MessagesReceived socket={socket} />
+                <SendMessage socket={socket} username={username} room={room} />
+            </div>
+        </div>
+    );
 };
 
-export default Chat;
+export default Chat;    
