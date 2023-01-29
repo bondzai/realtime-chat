c67ff0290c762fcb8dd5864a2d84477431c57d30
introbond <introbond.dev@gmail.com>
2023-01-29 12:07:50 +0700

    HarperDB data insert

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const cors = require('cors');
-const { Server } = require('socket.io'); // Add this
+const { Server } = require('socket.io');
+
+const harperSaveMessage = require('./services/harper-save-message'); // Add this    
 
 app.use(cors());
 
@@ -30,19 +34,19 @@ io.on('connection', (socket) => {
 
         console.log("[room]" + room)
         console.log("[chatRoom]" + chatRoom)
-        
+
         allUsers.push({ id: socket.id, username, room });
         chatRoomUsers = allUsers.filter((user) => user.room === room);
         socket.to(room).emit('chatroom_users', chatRoomUsers);
         socket.emit('chatroom_users', chatRoomUsers);
-        
+
         let __createdtime__ = Date.now();
-        
+
         socket.emit('receive_message', {
             message: `Welcome ${username}`,
             username: CHAT_BOT,
             __createdtime__,
-          });
+        });
 
         socket.to(room).emit('receive_message', {
             message: `${username} has joined the chat room`,
@@ -50,6 +54,15 @@ io.on('connection', (socket) => {
             __createdtime__,
         });
     });
+
+    socket.on('send_message', (data) => {
+        const { message, username, room, __createdtime__ } = data;
+        io.in(room).emit('receive_message', data); // Send to all users in room, including sender
+        harperSaveMessage(message, username, room, __createdtime__) // Save message in db
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err));
+    });
+
 });
 
 server.listen(4000, () => {
---
+++ backend/services/harper-get-message.js
@@ -0,0 +1,34 @@
+let axios = require('axios');
+
+function harperGetMessages(room) {
+    const dbUrl = process.env.HARPERDB_URL;
+    const dbPw = process.env.HARPERDB_PW;
+    if (!dbUrl || !dbPw) return null;
+
+    let data = JSON.stringify({
+        operation: 'sql',
+        sql: `SELECT * FROM realtime_chat.messages WHERE room = '${room}' LIMIT 100`,
+    });
+
+    let config = {
+        method: 'post',
+        url: dbUrl,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Basic ' + dbPw,
+        },
+        data: data,
+    };
+
+    return new Promise((resolve, reject) => {
+        axios(config)
+            .then(function (response) {
+                resolve(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                reject(error);
+            });
+    });
+}
+
+module.exports = harperGetMessages;
