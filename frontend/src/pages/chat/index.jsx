e28db92160fe1b3596060af9246050d5d93b0eb0
introbond <introbond.dev@gmail.com>
2023-01-29 12:27:27 +0700

    add chat board component

--- frontend/src/pages/chat/index.jsx
+++ frontend/src/pages/chat/index.jsx
@@ -1,16 +1,20 @@
 import styles from './styles.module.css';
+import RoomAndUsersColumn from './room-and-users'; // Add this
+import SendMessage from './send-message';
 import MessagesReceived from './messages';
-import SendMessage from './send_message';
 
 const Chat = ({ username, room, socket }) => {
-    return (
-        <div className={styles.chatContainer}>
-            <div>
-                <MessagesReceived socket={socket} />
-                <SendMessage socket={socket} username={username} room={room} />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.chatContainer}>
+      {/* Add this */}
+      <RoomAndUsersColumn socket={socket} username={username} room={room} />
+
+      <div>
+        <MessagesReceived socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
+      </div>
+    </div>
+  );
 };
 
-export default Chat;    
+export default Chat;
--- frontend/src/pages/chat/messages.jsx
+++ frontend/src/pages/chat/messages.jsx
@@ -1,8 +1,10 @@
 import styles from './styles.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Messages = ({ socket }) => {
   const [messagesRecieved, setMessagesReceived] = useState([]);
+
+  const messagesColumnRef = useRef(null); // Add this
 
   // Runs whenever a socket event is recieved from the server
   useEffect(() => {
@@ -18,9 +20,37 @@ const Messages = ({ socket }) => {
       ]);
     });
 
-	// Remove event listener on component unmount
+    // Remove event listener on component unmount
     return () => socket.off('receive_message');
   }, [socket]);
+
+  // Add this
+  useEffect(() => {
+    // Last 100 messages sent in the chat room (fetched from the db in backend)
+    socket.on('last_100_messages', (last100Messages) => {
+      console.log('Last 100 messages:', JSON.parse(last100Messages));
+      last100Messages = JSON.parse(last100Messages);
+      // Sort these messages by __createdtime__
+      last100Messages = sortMessagesByDate(last100Messages);
+      setMessagesReceived((state) => [...last100Messages, ...state]);
+    });
+
+    return () => socket.off('last_100_messages');
+  }, [socket]);
+
+  // Add this
+  // Scroll to the most recent message
+  useEffect(() => {
+    messagesColumnRef.current.scrollTop =
+      messagesColumnRef.current.scrollHeight;
+  }, [messagesRecieved]);
+
+  // Add this
+  function sortMessagesByDate(messages) {
+    return messages.sort(
+      (a, b) => parseInt(a.__createdtime__) - parseInt(b.__createdtime__)
+    );
+  }
 
   // dd/mm/yyyy, hh:mm:ss
   function formatDateFromTimestamp(timestamp) {
@@ -29,7 +59,8 @@ const Messages = ({ socket }) => {
   }
 
   return (
-    <div className={styles.messagesColumn}>
+    // Add ref to this div
+    <div className={styles.messagesColumn} ref={messagesColumnRef}>
       {messagesRecieved.map((msg, i) => (
         <div className={styles.message} key={i}>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
