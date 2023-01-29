d629bc804797adba568f0c4b26476e166021f7d7
introbond <introbond.dev@gmail.com>
2023-01-29 08:17:36 +0700

    [server] add api: 1st endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+  });
 
 module.exports = app;
