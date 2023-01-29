bf19b080fece279936358e460b0e59c95e74e37f
introbond <introbond.dev@gmail.com>
2023-01-29 09:34:45 +0700

    fixed: cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,34 @@
-app = require('./app');
-const PORT = process.env.PORT || 4001
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const cors = require('cors');
+const { Server } = require('socket.io'); // Add this
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port: ${PORT}`)
-})
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+app.get('/', (req, res) => {
+  res.send("test endpoint");
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:4000');
+});
