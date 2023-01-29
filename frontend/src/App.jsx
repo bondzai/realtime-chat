5a4f05c192e8a8775136cce5fed47e0d28d1c962
introbond <introbond.dev@gmail.com>
2023-01-29 08:04:06 +0700

    create: join room page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import Chat from "./components/Chat"
-import './App.css'
+
+import './App.css';
+import { useState } from 'react'; // Add this
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client'; // Add this
+import Home from './pages/home';
+
+const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [username, setUsername] = useState(''); // Add this
+    const [room, setRoom] = useState(''); // Add this
 
     return (
-        <div className="App">
-            <h2> chat room </h2>
-            <Chat/>
-        </div>
-    )
+        <Router>
+            <div className='App'>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <Home
+                                username={username} // Add this
+                                setUsername={setUsername} // Add this
+                                room={room} // Add this
+                                setRoom={setRoom} // Add this
+                                socket={socket} // Add this
+                            />
+                        }
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
