e18e1a78e280301aaa93db6f4634b34403456ef4
introbond <introbond.dev@gmail.com>
2023-01-29 09:34:57 +0700

    fixed: cors

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,17 +1,16 @@
 import styles from './styles.module.css';
-import { useNavigate } from 'react-router-dom'; // Add this
+import { useNavigate } from 'react-router-dom';
 
 const Home = ({ username, setUsername, room, setRoom, socket }) => {
 
-    const navigate = useNavigate(); // Add this
+    const navigate = useNavigate();
 
     const joinRoom = () => {
         if (room !== '' && username !== '') {
             socket.emit('join_room', { username, room });
         }
 
-        // Redirect to /chat
-        navigate('/chat', { replace: true }); // Add this
+        navigate('/chat', { replace: true });
 
     };
 
@@ -19,29 +18,16 @@ const Home = ({ username, setUsername, room, setRoom, socket }) => {
         <div className={styles.container}>
             <div className={styles.formContainer}>
                 <h1>{`<>DevRooms</>`}</h1>
-                <input className={styles.input} placeholder='Username...' />
-
-                <input
-                    className={styles.input}
-                    placeholder='Username...'
-                    onChange={(e) => setUsername(e.target.value)} // Add this
-                />
-
-                <select
-                    className={styles.input}
-                    onChange={(e) => setRoom(e.target.value)} // Add this
+                <input className={styles.input} placeholder='Username...' onChange={(e) => setUsername(e.target.value)} />
+                <select className={styles.input} onChange={(e) => setRoom(e.target.value)}> </select>
+                <button
+                    className='btn btn-secondary'
+                    style={{ width: '100%' }}
+                    onClick={joinRoom}
                 >
-                </select>
+                    Join Room
+                </button>
 
-                <select className={styles.input}>
-                    <option>-- Select Room --</option>
-                    <option value='javascript'>JavaScript</option>
-                    <option value='node'>Node</option>
-                    <option value='express'>Express</option>
-                    <option value='react'>React</option>
-                </select>
-
-                <button className='btn btn-secondary' style={{ width: '100%' }}>Join Room</button>
             </div>
         </div>
     );
