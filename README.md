d52ed449c8b535f29d51f4ea80a35c2415827024
introbond <introbond.dev@gmail.com>
2023-01-29 09:45:28 +0700

    added: chat bot

--- README.md
+++
@@ -1,7 +0,0 @@
-# realtime-chat-room
-
-> just a real time chat application buit with JavaScript stack.
-- frontend: Vite + React
-- backend: Node + Express
-- cloud: AWS (Cyclic)
-- database: ---
--- backend/index.js
+++ backend/index.js
@@ -8,27 +8,35 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173',
-      methods: ['GET', 'POST'],
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
     },
-  });
-
-app.get('/', (req, res) => {
-  res.send("test endpoint");
 });
 
+app.get('/', (req, res) => {
+    res.send("test endpoint");
+});
+
+const CHAT_BOT = 'ChatBot';
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log(`User connected ${socket.id}`);
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    // Add a user to a room
+    socket.on('join_room', (data) => {
+        const { username, room } = data; // Data sent from client when join_room event emitted
+        socket.join(room); // Join the user to a socket room
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+        // Add this
+        let __createdtime__ = Date.now(); // Current timestamp
+        // Send message to all users currently in the room, apart from the user that just joined
+        socket.to(room).emit('receive_message', {
+            message: `${username} has joined the chat room`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
+    });
 });
 
 server.listen(4000, () => {
-  console.log('listening on *:4000');
+    console.log('listening on *:4000');
 });
